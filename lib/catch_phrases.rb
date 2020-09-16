c1a2e3c6755be2c55455b6b4bcb4a525c3484b66
Christina Poole <codenamedchris@gmail.com>
2020-09-16 16:56:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,19 +3,19 @@ $status = "Thank You Mario! But Our Princess Is In Another Castle!"
 $link_phrase = "It's Dangerous To Go Alone! Take This."
 
 def mario
-  puts mario_phrase
+  puts $mario_phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
 
 def link
-  puts link_phrase
+  puts $link_phrase
 end
 
 def all_phrases
-  puts mario_phrase
-  puts status
-  puts link_phrase
+  puts $mario_phrase
+  puts $status
+  puts $link_phrase
 end
