677dc1a7889389f2cda7807b08ecbbce7e566ce2
Christina Poole <codenamedchris@gmail.com>
2020-09-16 16:54:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-mario_phrase = "It's-a-me, Mario!"
+mario_phrase = "It's-a me, Mario!"
 status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 link_phrase = "It's Dangerous To Go Alone! Take This."
 
